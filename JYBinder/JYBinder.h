a5d7ea52d01c28234a2922250a47b6368b4b1ea5
XuJiayao <xujiayao91@163.com>
2018-05-13 13:00:26 +0800

    simple

--- JYBinder/JYBinder.h
+++ JYBinder/JYBinder.h
@@ -8,7 +8,6 @@
 
 #import <Foundation/Foundation.h>
 #import "JYBinderTerminal.h"
-#import "JYBinderGenerator.h"
 
 /**
  生成单向绑定终端
@@ -18,7 +17,13 @@
  @return 终端
  */
 #define JYBindToSingleWay(TARGET, KEYPATH) \
-[[JYBinderGenerator alloc] initWithTarget:TARGET keyPath:@JYBinderKeypath(TARGET, KEYPATH) isTwoWay:NO][JYBinderKey]
+({ \
+JYBinder *binder = [[JYBinder alloc] init]; \
+[binder setValue:TARGET forKey:@"target"]; \
+[binder setValue:@JYBinderKeypath(TARGET, KEYPATH) forKey:@"keyPath"]; \
+[binder setValue:@(NO) forKey:@"isTwoWay"]; \
+binder;\
+})[nil]
 
 /**
  生成双向绑定终端
@@ -28,7 +33,13 @@
  @return 终端
  */
 #define JYBindToTwoWay(TARGET, KEYPATH) \
-[[JYBinderGenerator alloc] initWithTarget:TARGET keyPath:@JYBinderKeypath(TARGET, KEYPATH) isTwoWay:YES][JYBinderKey]
+({ \
+JYBinder *binder = [[JYBinder alloc] init]; \
+[binder setValue:TARGET forKey:@"target"]; \
+[binder setValue:@JYBinderKeypath(TARGET, KEYPATH) forKey:@"keyPath"]; \
+[binder setValue:@(YES) forKey:@"isTwoWay"]; \
+binder;\
+})[nil]
 
 /**
  将两终端解绑
@@ -39,20 +50,20 @@
  @param KEYPATH2 属性2
  */
 #define JYUnbind(TARGET1, KEYPATH1, TARGET2, KEYPATH2) \
-[JYBinderGenerator unbindWithTarget1:TARGET1 keyPath1:@JYBinderKeypath(TARGET1, KEYPATH1) target2:TARGET2 keyPath2:@JYBinderKeypath(TARGET2, KEYPATH2)]
+[JYBinder unbindWithTarget1:TARGET1 keyPath1:@JYBinderKeypath(TARGET1, KEYPATH1) target2:TARGET2 keyPath2:@JYBinderKeypath(TARGET2, KEYPATH2)]
 
 @interface JYBinder : NSObject
 
 /**
  将两终端做单向绑定
 
- @param leadingTerminal 监听者
- @param followingTerminal 跟随者
+ @param leadingTerminal 主动更新的终端
+ @param followingTerminal 被动更新的终端
  */
 + (void)bindToSingleWayWithLeading:(JYBinderTerminal *)leadingTerminal following:(JYBinderTerminal *)followingTerminal;
 
 /**
- 将两终端做双向绑定，互相监听跟随
+ 将两终端做双向绑定
 
  @param terminal 终端
  @param otherTerminal 另一个终端
@@ -76,5 +87,20 @@
  @param keyPath2 属性2
  */
 + (void)unbindWithTarget1:(__weak NSObject *)target1 keyPath1:(NSString *)keyPath1 target2:(__weak NSObject *)target2 keyPath2:(NSString *)keyPath2;
+
+#pragma mark - 私有方法，禁止直接调用
+
+/**
+ 主要起到两个作用：1、代码自动补齐。 2、返回属性名字符串
+ */
+#define JYBinderKeypath(OBJ, PATH) \
+(((void)(NO && ((void)OBJ.PATH, NO)), # PATH))
+
+/**
+ 下标语法回调
+ */
+- (JYBinderTerminal *)objectForKeyedSubscript:(NSString *)key;
+
+- (void)setObject:(JYBinderTerminal *)leadingTerminal forKeyedSubscript:(NSString *)key;
 
 @end
--- JYBinder/JYBinder.m
+++ JYBinder/JYBinder.m
@@ -7,23 +7,55 @@
 //
 
 #import "JYBinder.h"
+#import "JYBinderChannelManager.h"
+#import "JYBinderChannel.h"
+#import "JYBinderUtil.h"
+
+@interface JYBinder ()
+
+@property (nonatomic, weak) NSObject *target;
+
+@property (nonatomic, copy) NSString *keyPath;
+
+@property (nonatomic, assign) BOOL isTwoWay;
+
+@end
 
 @implementation JYBinder
 
 + (void)bindToSingleWayWithLeading:(JYBinderTerminal *)leadingTerminal following:(JYBinderTerminal *)followingTerminal {
-    [JYBinderGenerator bindToSingleWayWithLeading:leadingTerminal following:followingTerminal];
+    [[JYBinderChannelManager sharedInstance] addChannel:[[JYBinderChannel alloc] initSingleWayWithLeadingTerminal:leadingTerminal followingTerminal:followingTerminal]];
 }
 
 + (void)bindToTwoWayWithOneTerminal:(JYBinderTerminal *)terminal otherTerminal:(JYBinderTerminal *)otherTerminal {
-    [JYBinderGenerator bindToTwoWayWithOneTerminal:terminal otherTerminal:otherTerminal];
+    [[JYBinderChannelManager sharedInstance] addChannel:[[JYBinderChannel alloc] initTwoWayWithOneTerminal:terminal otherTerminal:otherTerminal]];
 }
 
 + (void)unbindWithOneTerminal:(JYBinderTerminal *)terminal otherTerminal:(JYBinderTerminal *)otherTerminal {
-    [JYBinderGenerator unbindWithOneTerminal:terminal otherTerminal:otherTerminal];
+    [[JYBinderChannelManager sharedInstance] removeChannel:[[JYBinderChannel alloc] initSingleWayWithLeadingTerminal:terminal followingTerminal:otherTerminal]];
 }
 
 + (void)unbindWithTarget1:(NSObject *__weak)target1 keyPath1:(NSString *)keyPath1 target2:(NSObject *__weak)target2 keyPath2:(NSString *)keyPath2 {
-    [JYBinderGenerator unbindWithTarget1:target1 keyPath1:keyPath1 target2:target2 keyPath2:keyPath2];
+    [self unbindWithOneTerminal:[[JYBinderTerminal alloc] initWithTarget:target1 keyPath:keyPath1] otherTerminal:[[JYBinderTerminal alloc] initWithTarget:target2 keyPath:keyPath2]];
+}
+
+#pragma mark - Key-Value
+
+- (JYBinderTerminal *)objectForKeyedSubscript:(NSString *)key {
+    JYBinderTerminal *leadingTerminal = [[JYBinderTerminal alloc] initWithTarget:self.target keyPath:self.keyPath];
+    return leadingTerminal;
+}
+
+- (void)setObject:(JYBinderTerminal *)leadingTerminal forKeyedSubscript:(NSString *)key {
+    NSAssert(![JYBinderUtil isObjectNull:leadingTerminal], @"【JYBinder】: leadingTerminal为空");
+    NSAssert([leadingTerminal isKindOfClass:[JYBinderTerminal class]], @"【JYBinder】: 无效的leadingTerminal");
+    
+    JYBinderTerminal *followingTerminal = [[JYBinderTerminal alloc] initWithTarget:self.target keyPath:self.keyPath];
+    if (self.isTwoWay) {
+        [JYBinder bindToTwoWayWithOneTerminal:leadingTerminal otherTerminal:followingTerminal];
+    } else {
+        [JYBinder bindToSingleWayWithLeading:leadingTerminal following:followingTerminal];
+    }
 }
 
 @end
